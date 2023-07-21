3452effacfbf6399dad0892b553610b92c38c970
Mecit Mert Bişgin <98218585+mecitmert@users.noreply.github.com>
2023-07-21 15:16:48 +0300

    Add files via upload
    Signed-off-by: Mecit Mert Bişgin <98218585+mecitmert@users.noreply.github.com>

---
+++ E_Bussiness_Facade_67/Program_67.cs
@@ -0,0 +1,10 @@
+﻿using E_Bussiness_Facade;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        CarFactory carFactory = new CarFactory();
+    }
+}
---
+++ E_Bussiness_Facade_67/undercarriage parts_67.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Bussiness_Facade
+{
+    public class undercarriage_parts//(alt takim)  yuruyen aksan sistem ve parcalari
+    {
+        public undercarriage_parts() 
+        {
+            Console.WriteLine("\nALT TAKIM SISTEMI VE ALT TAKIM PARCA BOLUMU");
+            Chassis();//Şasi
+            breakk();//fren
+            damper();//amortisör
+            wheel();//tekerlerk
+            Exhaust();//Egzoz
+            Differential();//Diferansiyel
+            Transmission();//vites kutusu
+
+
+        }
+
+        private void Exhaust()//Egzoz
+        {
+            Console.WriteLine(" Alt sisteme Egzoz monte edildi");
+        }
+        private void Transmission() //vites kutusu
+        {
+            Console.WriteLine("Sanziman kontrolu yapildi");
+        }
+        private void Chassis()//Şasi
+        {
+            Console.WriteLine("Şasi Oluşturuldu");
+        }
+        private void Differential()
+        {
+            Console.WriteLine("Diferansiyel ayarlari Yapildi");
+        }
+        private void damper()//amortisör
+        {
+            Console.WriteLine("Amortisör Şasiye eklendi");
+        }
+        private void wheel() //tekerlerk
+        {
+            Console.WriteLine("Tekerlekler Takildi");
+        }
+        private void breakk() //fren
+        {
+            Console.WriteLine("balatalar ve fren sistemi yerlestrirldi");
+        }
+
+    }
+}
